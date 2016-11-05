2774f53e8a31949b720e6def1ad2c5b08595dd60
Diego Li <282108074@qq.com>
2016-11-05 14:24:27 +0800

    Modify some details of ui and logic
    Add dynamic change in the view of shoe and make some compromises.
    Add custom view for main page.
    Add the highly user-defined operation about the shoes.
    Solve the lag problem of ModeView.

---
+++ Aurora/app/src/main/java/com/happylrd/aurora/todo/colorMode/ColorFolder.java
@@ -0,0 +1,62 @@
+package com.happylrd.aurora.todo.colorMode;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorFolder {
+    private String pattern;
+    private String animation;
+    private String rotation;
+    private String reaction;
+
+    private List<Integer> colorList;
+
+    public ColorFolder() {
+        colorList = new ArrayList<>();
+        colorList.add(Color.BLACK);
+        rotation = "Nothing";
+        animation = "Nothing";
+    }
+
+    public List<Integer> getColorList() {
+        return colorList;
+    }
+
+    public void setColorList(List<Integer> colorList) {
+        this.colorList = colorList;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getAnimation() {
+        return animation;
+    }
+
+    public void setAnimation(String animation) {
+        this.animation = animation;
+    }
+
+    public String getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(String rotation) {
+        this.rotation = rotation;
+    }
+
+    public String getReaction() {
+        return reaction;
+    }
+
+    public void setReaction(String reaction) {
+        this.reaction = reaction;
+    }
+}
